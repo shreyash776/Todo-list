2c7205e8bda1b70d659e7721eb06e2ebbb5034db
Priyanshu Saini <shreyashchaurasia77@gmail.com>
2023-09-19 23:17:59 +0530

    Change Names

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+/*
+  [
+    {
+      id:2,
+      value:3,
+      isCompeleted:true
+    }
+  ]
+ */
+
 let todosArr = [];
 let id = 0;
 
@@ -7,48 +17,74 @@ function createTodo() {
   if (input.value !== "") {
     id++;
     todosArr.push({
-      id:id,
+      id: id,
       value: input.value,
+      isCompleted: false,
     });
     input.value = "";
     displayTodo();
   }
 }
 
-
 // read array. (display todo)
 const displayTodo = () => {
   const todosListDiv = document.getElementById("todosList");
+  const completedTodosListDiv = document.getElementById("completedList");
   todosListDiv.innerHTML = ``;
+  completedTodosListDiv.innerHTML = ``;
 
   todosArr.forEach((todo) => {
-    todosListDiv.innerHTML += `
-    <div class="d-flex justify-content-between align-items-center">
-    <div class="d-flex align-items-center">
-      <div class="unChecked"></div>
-      <p>${todo.value}</p>
+    console.log(todo);
+    if (!todo.isCompleted) {
+      todosListDiv.innerHTML += `
+    <div class="box1">
+    <div class="text_box">
+      <input type="checkbox" name="todo" id="check${todo.id}" class="checked pointer" onclick="onCheckBoxChange(${todo.id})">
+     <label class=" value" for="check${todo.id}"> <p class="pointer">${todo.value}</p></label>
+    </div >
+    <div class="btn_box">
+      <button onclick='updateTodo(${todo.id})' class="btn btn-primary">Edit</button>
+      <button onclick='deleteTodo(${todo.id})'  class="btn btn-danger">Delete</button>
     </div>
-    <div>
-      <button onclick='updateTodo(${todo.id})'>Edit</button>
-      <button onclick='deleteTodo(${todo.id})'>Delete</button>
+    
+  </div>
+  <hr>
+    `;
+    } else {
+      completedTodosListDiv.innerHTML += `
+    <div class="box1">
+    <div class="text_box">
+      <input type="checkbox" name="todo" id="check${todo.id}" class="checked pointer" onclick="onCheckBoxChange(${todo.id})">
+     <label class=" value" for="check${todo.id}"> <p class="strike pointer">${todo.value}</p></label>
+    </div >
+    <div class="btn_box">
+      <button onclick='deleteTodo(${todo.id})'  class="btn btn-danger">Delete</button>
     </div>
   </div>
-  
     `;
+    }
   });
 };
 
-
 // delete todo
 const deleteTodo = (index) => {
-  9
   todosArr = todosArr.filter((todo) => {
     return todo.id != index;
   });
   displayTodo();
 };
 
+const onCheckBoxChange = (id) => {
+  const selectedTodo = todosArr.find((todo) => todo.id === id);
+  selectedTodo.isCompleted = !selectedTodo.isCompleted;
+  displayTodo();
 
+  // Update the checkbox state
+  const checkbox = document.getElementById(`check${id}`);
+  if (checkbox) {
+    checkbox.checked = selectedTodo.isCompleted;
+  }
+};
 
 // Update todo
 const updateTodo = (index) => {
@@ -57,24 +93,13 @@ const updateTodo = (index) => {
     todosArr.find((todo) => todo.id === index).value
   );
 
-  if (updatedValue !== null) { 
+  if (updatedValue !== null) {
     todosArr = todosArr.map((todo) => {
       if (todo.id === index) {
         todo.value = updatedValue;
       }
-      return todo;
+      //   return todo;
     });
     displayTodo();
   }
 };
-
-// const todos=[
-//   {
-//     id:1,
-//     value:'loer'
-//   },
-//   {
-//     id:2,
-//     value:'lorem'
-//   }
-// ]
